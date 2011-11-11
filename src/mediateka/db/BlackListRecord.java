53838c0682553b422ee83ff5c375c4f71d5eee5d
Alexandr <Alexandr@Alexandr-PC>
2011-11-11 23:57:04 +0400

    написаны BlackListRecord.java Disc.java, добавлен файл "РАЗДЕЛЕНИЕ КЛАССОВ.txt"

--- src/mediateka/db/BlackListRecord.java
+++ src/mediateka/db/BlackListRecord.java
@@ -1,76 +1,78 @@
 package mediateka.db;
 
-
-import mediateka.db.Record;
-import mediateka.db.Person;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.dom.DOMElement;
 
 /**
- * 
+ * Класс, представляющий запись в таблице "Черный список"
  * @author Alexandr
  */
 public class BlackListRecord implements Record {
 
-	private int recordID;
-	private Person person;
-	private String comment;
+    private int recordID;
+    private Person person;
+    private String comment;
 
-        /**
-         * 
-         * @return
-         */
-        public Person getPerson() {
-		return this.person;
-	}
-
-        /**
-         * 
-         * @param person
-         */
-        public void setPerson(Person person) {
-		this.person = person;
-	}
-
-        /**
-         * 
-         * @return
-         */
-        public String getComment() {
-		return this.comment;
-	}
-
-        /**
-         * 
-         * @param comment
-         */
-        public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	/**
-	 * 
-         * @param person
-         * @return  
-	 */
-	public BlackListRecord BlackList(Person person) {
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param person
-         * @param comment
-         * @return  
-	 */
-	public BlackListRecord BlackList(Person person, String comment) {
-		throw new UnsupportedOperationException();
-	}
-
-        /**
-         * 
-         * @return
-         */
-        public String ToXmlElement() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    /**
+     * Получить персональные данные
+     * @return Персональные данные
+     */
+    public Person getPerson() {
+        return this.person;
     }
 
+    /**
+     * Установить персональные данные
+     * @param person Персональные данные, которые будет установлены
+     */
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    /**
+     * Получить комментарий к записи
+     * @return Комментарий к записи
+     */
+    public String getComment() {
+        return this.comment;
+    }
+
+    /**
+     * Установить комментарий к записи
+     * @param comment Комментарий, который будет установлен
+     */
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    /**
+     * Конструктор с обязательным параметром
+     * @param person Персональные данные
+     */
+    public BlackListRecord(Person person) {
+        this.person = person;
+    }
+
+    /**
+     * Полный конструктор
+     * @param person Персональные данные
+     * @param comment Комментарий
+     */
+    public BlackListRecord(Person person, String comment) {
+        this.person = person;
+        this.comment = comment;
+    }
+
+    /**
+     * Сериализует запись в XML
+     * @return Строка с записью, сериализованной в XML element
+     */
+    public String ToXmlElement() {
+        Element elem = new DOMElement("record");
+        elem.addAttribute("recordID", Integer.toString(this.recordID));
+        elem.addElement("personID").addText(Integer.toString(this.person.getID()));
+        elem.addElement("comment").addText(this.comment);
+        return elem.asXML();
+    }
 }
--- src/mediateka/db/Disc.java
+++ src/mediateka/db/Disc.java
@@ -1,120 +1,161 @@
 package mediateka.db;
 
-
 import mediateka.db.Record;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.dom.DOMElement;
 
 /**
- * 
+ * Класс, представляющий диск с набором фильмов
  * @author Alexandr
  */
 public class Disc implements Record {
 
-	private int discID;
-	private Films films;
-	private int ownerID = 0;
-	private String format;
-	private int regionCode = 0;
-	private boolean isPresented = true;
+    /**
+     * Формат диска
+     */
+    public enum Format {
 
         /**
-         * 
-         * @return
+         * CD диск
          */
-        public Films getFilms() {
-		return this.films;
-	}
-
+        CD,
         /**
-         * 
-         * @param films
+         * DVD диск
          */
-        public void setFilms(Films films) {
-		this.films = films;
-	}
-
+        DVD,
         /**
-         * 
-         * @return
+         * Blue-Ray диск
          */
-        public int getOwnerID() {
-		return this.ownerID;
-	}
+        BD
+    };
+    private int discID;
+    private Films films;
+    private int ownerID = 0;
+    private Format format;
+    private int regionCode = 0;
+    private boolean isPresented = true;
 
-        /**
-         * 
-         * @return
-         */
-        public String getFormat() {
-		return this.format;
-	}
-
-        /**
-         * 
-         * @param format
-         */
-        public void setFormat(String format) {
-		this.format = format;
-	}
-
-        /**
-         * 
-         * @return
-         */
-        public int getRegionCode() {
-		return this.regionCode;
-	}
-
-        /**
-         * 
-         * @param regionCode
-         */
-        public void setRegionCode(int regionCode) {
-		this.regionCode = regionCode;
-	}
-
-        /**
-         * 
-         * @return
-         */
-        public boolean isIsPresented() {
-		return this.isPresented;
-	}
-
-        /**
-         * 
-         * @param isPresented
-         */
-        public void setIsPresented(boolean isPresented) {
-		this.isPresented = isPresented;
-	}
-
-	/**
-	 * 
-         * @param discFormat
-         * @return  
-	 */
-	public Disc Disc(String discFormat) {
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param owner
-	 * @param discFormat
-	 * @param discRegion
-         * @param presented
-         * @return  
-	 */
-	public Disc Disc(int owner, String discFormat, int discRegion, boolean presented) {
-		throw new UnsupportedOperationException();
-	}
-
-        /**
-         * 
-         * @return
-         */
-        public String ToXmlElement() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    /**
+     * Получить ID диска
+     * @return ID диска
+     */
+    public int getID() {
+        return this.discID;
     }
 
+    /**
+     * Получить все фильмы на диске
+     * @return Фильмы
+     */
+    public Films getFilms() {
+        return this.films;
+    }
+
+    /**
+     * Установить список фильмов
+     * @param films Фильмы которые будут установлены
+     */
+    public void setFilms(Films films) {
+        this.films = films;
+    }
+
+    /**
+     * Получить владельца диска
+     * @return Владелец диска
+     */
+    public int getOwnerID() {
+        return this.ownerID;
+    }
+
+    /**
+     * Получить формат диска
+     * @return Формат диска
+     */
+    public Format getFormat() {
+        return this.format;
+    }
+
+    /**
+     * Установить формат диска
+     * @param format Новый формат диска
+     */
+    public void setFormat(Format format) {
+        this.format = format;
+    }
+
+    /**
+     * Получить регион диска
+     * @return Регион диска
+     */
+    public int getRegionCode() {
+        return this.regionCode;
+    }
+
+    /**
+     * Установить регион диска
+     * @param regionCode Новый регион диска
+     */
+    public void setRegionCode(int regionCode) {
+        this.regionCode = regionCode;
+    }
+
+    /**
+     * В наличии ли диск
+     * @return Возвращает true если диск в наличии
+     */
+    public boolean isIsPresented() {
+        return this.isPresented;
+    }
+
+    /**
+     * Устанавливает наличие диска в коллекции
+     * @param isPresented Если диск в наличии - true, иначе false
+     */
+    public void setIsPresented(boolean isPresented) {
+        this.isPresented = isPresented;
+    }
+
+    /**
+     * Конструктор по обязательным параметрам
+     * @param format Формат диска
+     * @param films Список фильмов
+     */
+    public Disc(Format format, Films films) {
+        this(films, 0, format, 0, true);
+    }
+
+    /**
+     * Полный конструктор
+     * @param films Список фильмов
+     * @param ownerID Владелец диска
+     * @param format Формат диска
+     * @param discRegion Регион диска
+     * @param presented Наличие диска
+     */
+    public Disc(Films films, int ownerID, Format format, int discRegion, boolean presented) {
+        if ((format == null) || (films == null)) {
+            throw new NullPointerException();
+        }
+        this.films = films;
+        this.ownerID = ownerID;
+        this.format = format;
+        this.regionCode = discRegion;
+        this.isPresented = presented;
+    }
+
+    /**
+     * Сериализует диск в XML
+     * @return Строка с диском, сериализованным в XML element
+     */
+    public String ToXmlElement() {
+        Element elem = new DOMElement("disc");
+        elem.addAttribute("discID", Integer.toString(this.getID()));
+        elem.addElement("ownerID").addText(Integer.toString(this.getOwnerID()));
+        elem.addElement("format").addText(this.getFormat().toString());
+        elem.addElement("regionCode").addText(Integer.toString(this.getRegionCode()));
+        elem.addElement("isPresent").addText(Boolean.toString(this.isIsPresented()));
+        elem.addElement("films").addText(this.getFilms().ToXmlElement());
+        return elem.asXML();
+    }
 }
